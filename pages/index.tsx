69a85700b5b35997e529ae3d1855327022258f4b
blackrabbit <rabbitblack944@gmail.com>
2022-12-28 22:00:03 +0800

    测试

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from '../styles/Home.module.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
     return (
@@ -15,7 +15,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className={styles.main}>
-                <h1 className="text-3xl font-bold underline">Hello world!</h1>
+                <h1 className="text-3xl font-bold underline">Hello world!123</h1>
             </main>
         </>
     );
